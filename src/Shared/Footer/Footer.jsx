149177b3d05fc35e6316acc738937f76de387b46
tonni-akter-dev <tanniakhter123@gmail.com>
2024-02-07 18:11:04 +0600

    footer added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -5,21 +5,21 @@ export const Footer = () => {
         <>
             <div className='bg-[#100425] pt-[80px] pb-[48px] text-white  '>
                 <div className='max-w-[1280px] w-full mx-auto '>
-                    <div className='flex justify-between mb-[36px]'>
+                    <div className='flex lg:flex-row flex-col lg:justify-between justify-center mb-[36px]'>
 
-                        <div className='flex flex-col gap-[32px] items-start'>
+                        <div className='flex flex-col lg:items-start items-center justify-center gap-[32px] '>
                             <img src={logo} alt="logo" />
-                            <ul className='flex gap-[32px] text-base '>
+                            <ul className='flex gap-[32px] lg:flex-row flex-col text-base '>
                                 <li>Show Time</li>
                                 <li>Booking</li>
                                 <li>About Us</li>
                             </ul>
                         </div>
 
-                        <div>
+                        <div className='lg:text-start text-center lg:pt-0  pt-[30px]'>
                             <p className='text-sm font-semibold mb-[16px]'>Stay up to date</p>
 
-                            <div className="flex gap-[16px]">
+                            <div className="flex lg:flex-row flex-col lg:items-start items-center gap-[16px]">
                                 <input type="email" className='bg-white py-[10px] px-[14px] shadow-input rounded-[8px] border border-[#D0D5DD] ' placeholder="Enter your email" name="" id="" />
 
                                 <button className='text-white max-w-[200px]  px-[16px]  w-full bg-[#FF3D48] border border-[#FF3D48] shadow-button py-[12px] rounded-[8px] '>Subscribe</button>
@@ -28,7 +28,7 @@ export const Footer = () => {
 
                     </div>
                     <div className='h-[1px] bg-[#572314] mb-[32px]'></div>
-                    <div className='flex justify-between text-[#E2E8F0]'>
+                    <div className='flex lg:justify-between   justify-center lg:items-start items-center lg:flex-row flex-col  text-[#E2E8F0] lg:gap-0 gap-[30px]'>
                         <p className=''>© 2024 Shymoli Cinema Hall. All rights reserved.</p>
                         <ul className='flex gap-[16px] text-base font-normal'>
                             <li>Show Time</li>
--- src/components/Home/Connect.jsx
+++ src/components/Home/Connect.jsx
@@ -7,53 +7,52 @@ import location from '../../assets/location.png'
 const Connect = () => {
     return (
         <>
-            <div className='connect_page'>
-                <div className=''>
-                    <div className="flex lg:flex-row flex-col  justify-center items-center">
+            <div className='connect_page '>
 
-                        <div className='text-white text-base font-medium flex flex-col gap-[12px] lg:w-[487px] w-full lg:mb-0 mb-[50px] '>
-                            <h3 className='text-white text-[36px] font-bold mb-[24px]'>Let’s connect</h3>
-                            <div className='flex  gap-[12px]'>
-                                <img src={phone} alt="" />
-                                <p>02-581-51-775</p>
-                            </div>
-                            <div className='flex  gap-[12px]'>
-                                <img src={email} alt="" />
-                                <p>shyamolicinema@gmail.com</p>
-                            </div>
-                            <div className='flex  gap-[12px]'>
-                                <img src={location} alt="" />
-                                <p>Shymoli Square Shopping Mall, Mirpur Rd, Dhaka</p>
-                            </div>
+                <div className="flex lg:flex-row flex-col  justify-center items-center">
+
+                    <div className='text-white text-base font-medium flex flex-col lg:px-0 px-[20px] gap-[12px] lg:w-[487px] w-full lg:mb-0 mb-[50px] '>
+                        <h3 className='text-white text-[36px] font-bold mb-[24px]'>Let’s connect</h3>
+                        <div className='flex items-center  gap-[12px]'>
+                            <img src={phone} alt="" />
+                            <p>02-581-51-775</p>
+                        </div>
+                        <div className='flex items-center gap-[12px]'>
+                            <img src={email} alt="" />
+                            <p>shyamolicinema@gmail.com</p>
+                        </div>
+                        <div className='flex  items-center gap-[12px]'>
+                            <img src={location} alt="" />
+                            <p>Shymoli Square Shopping Mall, Mirpur Rd, Dhaka</p>
+                        </div>
+                    </div>
+
+                    <form className='lg:p-[40px] p-[20px] lg:mx-0 mx-[20px]  contact_form  flex flex-col gap-[24px] text-white '>
+
+                        <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%] '>
+                            <label className='text-sm text-[#E2E8F0]  '>Your name</label>
+                            <input type="text" className='ps-[10px]  bg-transparent border rounded-[6px] h-[48px] border-[#CBD5E1] ' />
+                        </div>
+                        <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%]  '>
+                            <label className='text-sm text-[#E2E8F0]  '>Email address</label>
+                            <input type="email" className='ps-[10px]  bg-transparent border rounded-[6px] h-[48px] border-[#CBD5E1] ' />
+                        </div>
+                        <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%]  '>
+                            <label className='text-sm text-[#E2E8F0]'>Phone number</label>
+                            <input type="number" className='ps-[10px]  bg-transparent border rounded-[6px] h-[48px] border-[#CBD5E1] ' />
+                        </div>
+                        <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%]  '>
+                            <label className='text-sm text-[#E2E8F0]'>Message</label>
+
+                            <textarea name="" id="" cols="30" rows="10" className='ps-[10px]  bg-transparent border rounded-[6px] h-[112px] border-[#CBD5E1]'></textarea>
                         </div>
 
 
-                        <form className='lg:p-[40px] p-[20px] contact_form flex flex-col gap-[24px] text-white '>
+                        <button className='text-white max-w-[200px] w-full bg-[#FF3D48] border border-[#FF3D48] shadow-button py-[12px] rounded-[8px] '>Send Message</button>
+                    </form>
 
-                            <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%] '>
-                                <label className='text-sm text-[#E2E8F0]  '>Your name</label>
-                                <input type="text" className='ps-[10px]  bg-transparent border rounded-[6px] h-[48px] border-[#CBD5E1] ' />
-                            </div>
-                            <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%]  '>
-                                <label className='text-sm text-[#E2E8F0]  '>Email address</label>
-                                <input type="email" className='ps-[10px]  bg-transparent border rounded-[6px] h-[48px] border-[#CBD5E1] ' />
-                            </div>
-                            <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%]  '>
-                                <label className='text-sm text-[#E2E8F0]'>Phone number</label>
-                                <input type="number" className='ps-[10px]  bg-transparent border rounded-[6px] h-[48px] border-[#CBD5E1] ' />
-                            </div>
-                            <div className='flex flex-col gap-[4px] lg:w-[488px] w-[100%]  '>
-                                <label className='text-sm text-[#E2E8F0]'>Message</label>
-
-                                <textarea name="" id="" cols="30" rows="10" className='ps-[10px]  bg-transparent border rounded-[6px] h-[112px] border-[#CBD5E1]'></textarea>
-                            </div>
-
-
-                            <button className='text-white max-w-[200px] w-full bg-[#FF3D48] border border-[#FF3D48] shadow-button py-[12px] rounded-[8px] '>Send Message</button>
-                        </form>
-
-                    </div>
                 </div>
+
             </div>
 
         </>
