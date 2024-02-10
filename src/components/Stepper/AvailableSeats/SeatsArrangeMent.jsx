508afc5ef749d4e23dbbf4f12aaf168b54b8080c
tonni-akter-dev <tanniakhter123@gmail.com>
2024-02-10 18:25:15 +0600

    verify number details add

--- src/components/Stepper/AvailableSeats/SeatsArrangeMent.jsx
+++ src/components/Stepper/AvailableSeats/SeatsArrangeMent.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
-
+import Seats from '../../../utils/seats'
 export const SeatsArrangeMent = () => {
+  console.log(Seats)
   return (
-    <div>SeatsArrangeMent</div>
+    <div>
+      {/* <div className="grid grid-cols-12">
+        <div className="col-span-6">
+
+          <div className='grid grid-cols-12 gap-[12px]'>
+            <div className="col-span-1">
+              <div className='w-[66px] h-[48px] bg-[#3D3D3D] rounded-[4px] opacity-[0.6] flex justify-center items-center text-[20px] font-medium'>
+                F1
+              </div>
+              
+            </div>
+            <div className="col-span-1">
+              <div className='w-[66px] h-[48px] bg-[#3D3D3D] rounded-[4px] opacity-[0.6] flex justify-center items-center text-[20px] font-medium  '>
+                F1
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>05</div>
+        <div className="grid col-span-3 gap-4 grid-cols-subgrid">
+          <div className="">06</div>
+          <div className="">06</div>
+          <div className="">06</div>
+        </div>
+        <div className="grid col-span-3 gap-4 grid-cols-subgrid">
+          <div className="">06</div>
+          <div className="">06</div>
+          <div className="">06</div>
+          <div className="">06</div>
+          <div className="">06</div>
+          <div className="">06</div>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ src/utils/seats.js
@@ -0,0 +1,484 @@
+const Seats = [
+    {
+        id: 1,
+        seatNumber: "F1",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F3",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F2",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F4",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F5",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F8",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F6",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F9",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F7",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F10",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F11",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F17",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F12",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F18",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F13",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F19",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F14",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F20",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F15",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F21",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F16",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F22",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "E1",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F1",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "E2",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F2",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "E3",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F3",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "E4",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F4",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "E5",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F5",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "E6",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F6",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F7",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F7",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F8",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F8",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F9",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F9",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F10",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F10",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F11",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F11",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F12",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F12",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F13",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F13",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F14",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F14",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F15",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F15",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F16",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F16",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F17",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F17",
+        status: "available"
+    },
+
+    {
+        id: 2,
+        seatNumber: "F18",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F18",
+        status: "available"
+    },
+    // 
+    {
+        id: 2,
+        seatNumber: "F1",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F1",
+        status: "available"
+    },
+
+    {
+        id: 2,
+        seatNumber: "F2",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F2",
+        status: "available"
+    },
+
+    {
+        id: 2,
+        seatNumber: "F3",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F3",
+        status: "available"
+    },
+
+    {
+        id: 2,
+        seatNumber: "F4",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F4",
+        status: "available"
+    },
+
+    {
+        id: 2,
+        seatNumber: "F5",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F5",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F6",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F6",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F7",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F7",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F8",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F8",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F9",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F9",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F10",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F10",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F11",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F11",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F12",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F12",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F13",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F13",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F14",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F14",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F15",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F15",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F16",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F16",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F17",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F17",
+        status: "available"
+    },
+
+    {
+        id: 2,
+        seatNumber: "F18",
+        status: "available"
+    },
+    {
+        id: 2,
+        seatNumber: "F18",
+        status: "available"
+    },
+
+
+
+]
+
+export default Seats
