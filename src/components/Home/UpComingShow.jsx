6e52aaf54cf74bf419ca9386ae3dde3568e96237
tonni-akter-dev <tanniakhter123@gmail.com>
2024-02-13 17:33:02 +0600

    upcoming show slider added

--- src/components/Home/UpComingShow.jsx
+++ src/components/Home/UpComingShow.jsx
@@ -4,6 +4,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import show1 from '../../assets/ushow.png'
+import show2 from '../../assets/ushow2.png'
+import show3 from '../../assets/ushow3.png'
 
 // import required modules
 import { Pagination, Navigation, HashNavigation, Autoplay } from 'swiper/modules';
@@ -15,32 +18,93 @@ const UpComingShow = () => {
 
             <div className='max-w-[1280px] w-full mx-auto'>
                 <h1 className='text-primary text-[36px] font-bold text-center'>Upcoming Shows</h1>
-
-                {/* <div className='bg-[#0F0F0F] h-[56px] w-[134px] absolute right-[376px] top-[69px] lg:block hidden '></div> */}
+               
                 <Swiper
                     spaceBetween={30}
                     navigation={true}
                     modules={[Pagination, Navigation, HashNavigation, Autoplay]}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
-                    className="mySwiper text-white h-[800px]"
+                    // autoplay={{
+                    //     delay: 2500,
+                    //     disableOnInteraction: false,
+                    // }}
+                    breakpoints={{
+                        640: {
+                          slidesPerView: 1,
+                          spaceBetween: 20,
+                        },
+                        768: {
+                          slidesPerView:2,
+                          spaceBetween: 40,
+                        },
+                        1024: {
+                          slidesPerView: 3,
+                          spaceBetween: 50,
+                        },
+                      }}
+                    className="mySwiper text-white h-[700px] lg:h-[800px]"
                 >
-                    <SwiperSlide data-hash="slide1" className='pt-[100px]'>
-
-                        <UpComingShowCards />
-                    </SwiperSlide>
-                    <SwiperSlide data-hash="slide1" className='pt-[100px]'>
-
-                        <UpComingShowCards />
-                    </SwiperSlide>
-                    <SwiperSlide data-hash="slide1" className='pt-[100px]'>
-
-                        <UpComingShowCards />
-                    </SwiperSlide>
-
-
+                    <div className="grid grid-cols-12">
+                        <SwiperSlide data-hash="slide1" className='lg:pt-[100px] pt-[24px]'>
+                            <div className="col-span-12 lg:col-span-4 lg:mx-0 mx-[24px]">
+                                <div>
+                                    <img className='mb-[24px]' src={show1} alt="" />
+                                    <p className='text-[20px] font-bold text-white mb-[12px] '>Fighter</p>
+                                    <p className='text-base font-normal text-[#94A3B8] mb-[8px]'>2h 15m • Movie • Thriller/Mystery</p>
+                                    <p className='text-base font-normal text-[#94A3B8]'>Released : December 24, 2023</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide data-hash="slide1" className='lg:pt-[100px] pt-[24px]'>
+                            <div className="col-span-12 lg:col-span-4 lg:mx-0 mx-[24px]">
+                                <div>
+                                    <img className='mb-[24px]' src={show2} alt="" />
+                                    <p className='text-[20px] font-bold text-white mb-[12px] '>Fighter</p>
+                                    <p className='text-base font-normal text-[#94A3B8] mb-[8px]'>2h 15m • Movie • Thriller/Mystery</p>
+                                    <p className='text-base font-normal text-[#94A3B8]'>Released : December 24, 2023</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide data-hash="slide1" className='lg:pt-[100px] pt-[24px]'>
+                            <div className="col-span-12 lg:col-span-4 lg:mx-0 mx-[24px]">
+                                <div>
+                                    <img className='mb-[24px]' src={show3} alt="" />
+                                    <p className='text-[20px] font-bold text-white mb-[12px] '>Fighter</p>
+                                    <p className='text-base font-normal text-[#94A3B8] mb-[8px]'>2h 15m • Movie • Thriller/Mystery</p>
+                                    <p className='text-base font-normal text-[#94A3B8]'>Released : December 24, 2023</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide data-hash="slide1" className='lg:pt-[100px] pt-[24px]'>
+                            <div className="col-span-12 lg:col-span-4 lg:mx-0 mx-[24px]">
+                                <div>
+                                    <img className='mb-[24px]' src={show1} alt="" />
+                                    <p className='text-[20px] font-bold text-white mb-[12px] '>Fighter</p>
+                                    <p className='text-base font-normal text-[#94A3B8] mb-[8px]'>2h 15m • Movie • Thriller/Mystery</p>
+                                    <p className='text-base font-normal text-[#94A3B8]'>Released : December 24, 2023</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide data-hash="slide1" className='lg:pt-[100px] pt-[24px]'>
+                            <div className="col-span-12 lg:col-span-4 lg:mx-0 mx-[24px]">
+                                <div>
+                                    <img className='mb-[24px]' src={show2} alt="" />
+                                    <p className='text-[20px] font-bold text-white mb-[12px] '>Fighter</p>
+                                    <p className='text-base font-normal text-[#94A3B8] mb-[8px]'>2h 15m • Movie • Thriller/Mystery</p>
+                                    <p className='text-base font-normal text-[#94A3B8]'>Released : December 24, 2023</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide data-hash="slide1" className='lg:pt-[100px] pt-[24px]'>
+                            <div className="col-span-12 lg:col-span-4 lg:mx-0 mx-[24px]">
+                                <div>
+                                    <img className='mb-[24px]' src={show3} alt="" />
+                                    <p className='text-[20px] font-bold text-white mb-[12px] '>Fighter</p>
+                                    <p className='text-base font-normal text-[#94A3B8] mb-[8px]'>2h 15m • Movie • Thriller/Mystery</p>
+                                    <p className='text-base font-normal text-[#94A3B8]'>Released : December 24, 2023</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    </div>
                 </Swiper>
 
             </div>
