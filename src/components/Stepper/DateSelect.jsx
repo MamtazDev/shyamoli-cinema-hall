f57699eed5e2e0b8a7e7307c2c98dde034dd3c5b
tonni-akter-dev <tanniakhter123@gmail.com>
2024-02-14 18:31:29 +0600

    add animation

--- src/components/Stepper/DateSelect.jsx
+++ src/components/Stepper/DateSelect.jsx
@@ -61,73 +61,119 @@
 // export default DateSelect
 
 import React, { useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import Slider from "react-slick";
 
 const DateSelect = () => {
-
+    var settings = {
+        // dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
     const [activeIndex, setActiveIndex] = useState(0);
 
     const handleActive = (index) => {
         setActiveIndex(index);
     };
+
     return (
         <div className='mt-[28px] date_select_slider '>
-            <Swiper
-                navigation={true}
-                modules={[Pagination, Navigation, Autoplay]}
-                className='mySwiper'
-                //   autoplay={{ delay: 3000 }}
-                breakpoints={{
-                    640: {
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                    },
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 40,
-                    },
-                    1024: {
-                        slidesPerView: 7,
-                        spaceBetween: 10,
-                    },
-                }}
-            >
-                {[...Array(10)].map((_, index) => (
-                    <SwiperSlide key={index} className='w-[99px]'>
-                        <div
-                            onClick={() => handleActive(index)}
-                            className={`h-[108px] w-[99px] rounded-[8px] cursor-pointer p-[10px] text-center ${activeIndex === index
-                                ? 'bg-white'
-                                : 'border-[2px] border-[#A6A6A6]'
-                                }`}
-                        >
-                            <p
-                                className={`text-base font-medium leading-[24px] ${activeIndex === index ? 'text-[#2B2B2B]' : 'text-[#E2E8F0]'
+
+            {/* {[...Array(10)].map((_, index) => ( */}
+            {/* <div key={index} className=''>
+                <div
+                    onClick={() => handleActive(index)}
+                    className={` rounded-[8px]  h-[108px] w-[99px]cursor-pointer p-[10px] text-center ${activeIndex === index
+                        ? 'bg-white'
+                        : 'border-[2px] border-[#A6A6A6]'
+                        }`}
+                >
+                    <p
+                        className={`text-base font-medium leading-[24px] ${activeIndex === index ? 'text-[#2B2B2B]' : 'text-[#E2E8F0]'
+                            }`}
+                    >
+                        Thr
+                    </p>
+                    <h3
+                        className={`text-[24px] ${activeIndex === index ? 'text-[#2B2B2B]' : 'text-[#E2E8F0]'
+                            } font-bold leading-[32px] `}
+                    >
+                        25
+                    </h3>
+                    <p
+                        className={`text-base font-normal leading-[24px] ${activeIndex === index ? 'text-[#2B2B2BBF]' : 'text-[#CBD5E1]'
+                            } `}
+                    >
+                        January
+                    </p>
+                </div>
+            </div> */}
+            <div className="slider-container">
+                <Slider {...settings}>
+
+                    {[...Array(10)].map((_, index) => (
+                        <>
+                            <div
+                                onClick={() => handleActive()}
+                                className={` rounded-[8px]  h-[108px] w-[99px] cursor-pointer p-[10px] text-center ${activeIndex === index
+                                    ? 'bg-white'
+                                    : 'border-[2px] border-[#A6A6A6]'
                                     }`}
                             >
-                                Thr
-                            </p>
-                            <h3
-                                className={`text-[24px] ${activeIndex === index ? 'text-[#2B2B2B]' : 'text-[#E2E8F0]'
-                                    } font-bold leading-[32px] `}
-                            >
-                                25
-                            </h3>
-                            <p
-                                className={`text-base font-normal leading-[24px] ${activeIndex === index ? 'text-[#2B2B2BBF]' : 'text-[#CBD5E1]'
-                                    } `}
-                            >
-                                January
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+                                <p
+                                    className={`text-base font-medium leading-[24px] ${activeIndex === index ? 'text-[#2B2B2B]' : 'text-[#E2E8F0]'
+                                        }`}
+                                >
+                                    Thr
+                                </p>
+                                <h3
+                                    className={`text-[24px] ${activeIndex === index ? 'text-[#2B2B2B]' : 'text-[#E2E8F0]'
+                                        } font-bold leading-[32px] `}
+                                >
+                                    25
+                                </h3>
+                                <p
+                                    className={`text-base font-normal leading-[24px] ${activeIndex === index ? 'text-[#2B2B2BBF]' : 'text-[#CBD5E1]'
+                                        } `}
+                                >
+                                    January
+                                </p>
+                            </div>
+
+                        </>
+                    ))}
+                </Slider>
+            </div>
+
+        </div >
     )
 }
 
